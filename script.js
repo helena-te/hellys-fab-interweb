96131758d2e3c74b70c8dcc47b582e149f6bcc3e
Helena Tenev <htenev14@gmail.com>
2022-11-10 15:18:03 -0500

    fix amor artis link part 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const p1 = document.createElement("p1");
-p1.textContent = "Helena grew up in New Jersey and graduated from Princeton Univeristy with a concentration in Mathematics and a certificate in Vocal Consort Singing. She is learning to code. She loves to sing with her choir, Amor Artis, skiing, playing with her cousins' kids, having conversations with her friends in parked cars, and glitter.";
-document.querySelector("body").appendChild(p1);
+//const p1 = document.createElement("p1");
+//p1.textContent = "Helena grew up in New Jersey and graduated from Princeton Univeristy with a concentration in Mathematics and a certificate in Vocal Consort Singing. She is learning to code. She loves to sing with her choir, Amor Artis, skiing, playing with her cousins' kids, having conversations with her friends in parked cars, and glitter.";
+//document.querySelector("body").appendChild(p1);
 function addingEventListener() {const input = document.getElementById('button');
 
 function clickAlert() {
