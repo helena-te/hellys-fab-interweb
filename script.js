8a9a71f25266b38f6c1a19939aae0555a143d457
Helena Tenev <htenev14@gmail.com>
2022-11-10 23:08:21 -0500

    make alert work attempt 2

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 //const p1 = document.createElement("p1");
 //p1.textContent = "Helena grew up in New Jersey and graduated from Princeton Univeristy with a concentration in Mathematics and a certificate in Vocal Consort Singing. She is learning to code. She loves to sing with her choir, Amor Artis, skiing, playing with her cousins' kids, having conversations with her friends in parked cars, and glitter.";
 //document.querySelector("body").appendChild(p1);
-function addingEventListener() {const input = document.getElementById('button');
+const input = document.getElementById('button');
 
 function clickAlert() {
   alert('fun fact!');
 }
 
-input.addEventListener('click', clickAlert);}
-
-addingEventListener()
+input.addEventListener('click', clickAlert);
 
 var canvas;
 var stage;
